320d3affc99214079c080a4e14ae285d79921c71
LuisVynicius <luismevy@gmail.com>
2024-06-06 13:37:06 -0300

    Data Structure: Some fixes

--- Data_Structure/LinkedList/Main.java
+++ Data_Structure/LinkedList/Main.java
@@ -3,19 +3,10 @@ package Data_Structure.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        Long ft = System.currentTimeMillis();
         LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < 1000000; i++) {
             list.add(i);
         }
-        System.out.println("Tempo01: " + (System.currentTimeMillis() - ft));
-        
-        Long st = System.currentTimeMillis();
-        java.util.LinkedList<Integer> list2 =  new java.util.LinkedList<>();
-        for (int i = 0; i < 1000000; i++) {
-            list2.add(i);
-        }
-        System.out.println("Tempo02: " + (System.currentTimeMillis() - st));
         
     }
 }
